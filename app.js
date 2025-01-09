c1df2baaf5e08c6954d26100d9607f27e0d55220
remodelros <remonster510@gmail.com>
2025-01-10 00:54:05 +0800

    asd

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ const fileUpload = require("express-fileupload");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-
-}));
+app.use(cors());
+app.options('*', cors())
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(
