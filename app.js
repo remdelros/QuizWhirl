4de4fd0c0cba6a390d2c1ba305f11853a2f3b274
remodelros <remonster510@gmail.com>
2025-01-10 10:24:27 +0800

    hotdog

--- app.js
+++ app.js
@@ -34,6 +34,17 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 // OpenAI Configuration
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
